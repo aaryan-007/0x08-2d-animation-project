6e0a7f9fb7e71ec3f896beae69e3c3cbe6bc1550
AaryanDuggal <66437189+aaryan-007@users.noreply.github.com>
2024-04-22 01:07:29 +0530

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1,59 +1,21 @@
+import Domel from './domel.js';
+
 class Experiment {
-  // Group Details
-  static rollNos = '10983437,10983743'
-  static names = 'The Tutors(Akhtar Banga, Phul Tekchand)'
+  // Candidate Details
+  static rollNo = '102103449,102103436'
+  static name = 'Aaryan Duggal, Jaideep Singh'
 
-  canvasSel = '#myCanvas'
-
-  run() {
-
-    // Run the Steppers
-    // this.runSteppers()
-
-    // Hide Steppers
-    this.hideSteppers()
-    canvasSetup(this.canvasSel)
-
-    // Clock
-    // --------------------------------------------------
-    const clock = new Clock(this.canvasSel)
-    // const ms = document.timeline.currentTime
-    // clock.draw(ms)
-    // clock.draw(ms+25000)
-    const clockRafFn = (ts) => {
-      clock.draw(ts)
-      window.requestAnimationFrame(clockRafFn)
-    }
-    const clockRaf = window.requestAnimationFrame(clockRafFn)
-    
+  constructor() {
+    this.canvasSelector = '#canvas';
+    this.domel = new Domel(this.canvasSelector);
   }
 
-  runSteppers() {
-
-    // Steppers
-    // --------------------------------------------------
-    const stepperOneRaf
-	  = window.requestAnimationFrame(stepperOne)
-
-    const stepperTwoRaf
-	  = window.requestAnimationFrame(stepperTwo)
-
-    const stepperThree = new StepperThree(
-      '#valueFromStepperThree', 15
-    )
-    const stepperThreeFn = (ts) => {
-      if (!stepperThree.isActive) stepperThree.start()
-      stepperThree.step(ts)
-      window.requestAnimationFrame(stepperThreeFn)
-    }
-    const stepperThreeRaf
-	  = window.requestAnimationFrame(stepperThreeFn)
-    // --------------------------------------------------
-    
-  }
-
-  hideSteppers() {
-    document.querySelector('#side-note')
-      .classList.add('hidden')
+  async setup() {
+    // Initialize the Domel object
+    await this.domel.setup();
   }
 }
+
+// Initialize the experiment
+const experiment = new Experiment();
+experiment.setup();
